122cdd04534d5e0b7618acc1f1de109756bf901a
Carlos Jenkins <carlos.jenkins@hpe.com>
2025-11-04 22:48:57 -0600

    Adds a testing suite to test from Python 3.8 up to 3.14.

---
+++ test/testpkg/testpkg/__init__.py
@@ -0,0 +1,8 @@
+def test():
+    return 'This is a test function from testpkg.'
+
+def plugin1():
+    return 'Hello from testpkg plugin plugin1!'
+
+def plugin2():
+    return 'Hello from testpkg plugin plugin2!'
